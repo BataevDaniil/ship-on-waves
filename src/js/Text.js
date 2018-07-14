001c5da7d92e4d34e4b126b4c405f1496e72890a
BataevDaniil <ragoga48@gmail.com>
2018-07-14 13:49:01 +0300

    Add mode edit for Texts.

--- src/js/Text.js
+++ src/js/Text.js
@@ -2,18 +2,28 @@ class Text {
 	constructor(insert, point, text) {
 		this.point = point;
 		this.insert = insert;
-		this.element = document.createElement('div');
-		this.element.setAttribute('class', 'text');
-		this.element.innerText = text;
-		this.element.addEventListener('mousemove', this.handlerMouseDown);
+		this.element = document.createElement('input');
+		this.noneActive();
+		this.element.value = text;
 		this.insert.appendChild(this.element);
 
 		this.element.style = `
 			left: ${point.x - this.element.offsetWidth / 2}px;
 			bottom: ${point.y - this.element.offsetHeight - 9 - 12}px;
 		`;
-		console.log();
+		this.element.addEventListener('mousemove', this.handlerMouseDown);
 	}
+
+	active = () =>
+		this.element.setAttribute('class', 'text text-active');
+	noneActive = () =>
+		this.element.setAttribute('class', 'text');
+
+	disabled = () =>
+		this.element.setAttribute('disabled', '');
+
+	noneDisabled = () =>
+		this.element.removeAttribute('disabled', '');
 
 	setPoint(value) {
 		this.point = value;
